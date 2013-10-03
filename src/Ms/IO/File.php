09b8d26e4d022084fd56e460a6a22d60316b3631
ibm-ibm <dev@mangos.fr>
2013-10-03 04:14:11 +0200

    Maj

--- src/Ms/IO/File.php
+++ src/Ms/IO/File.php
@@ -1,4 +1,11 @@
 <?php
+/**
+* Mangos File Class
+*
+* @package	      Mangos Framework
+* @author             Mangos Team Dev
+* @copyright          Copyright (c) 2013, Mangos.fr, Inc. (http://framework.mangos.fr/)
+*/
 
 class File 
 {
