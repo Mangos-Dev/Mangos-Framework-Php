2147421ab44185f398d249f014331da34937f13f
ibm-ibm <dev@mangos.fr>
2013-10-03 04:10:58 +0200

    Maj

--- src/Ms/IO/Directory.php
+++ src/Ms/IO/Directory.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+* Mangos Directory Class
+*
+* @package	      Mangos Framework
+* @author             Mangos Team Dev
+* @copyright          Copyright (c) 2013, Mangos.fr, Inc. (http://framework.mangos.fr/)
+*/
 
 class Directory 
 {
